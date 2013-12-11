4eca3175f4a3bed63dd6c62ee53f44162d453730
ktryber <Ktryber@gmail.com>
2013-12-11 14:42:18 -0500

    udpated urls

--- wp-config.php
+++ wp-config.php
@@ -99,3 +99,6 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+  define('WP_HOME','http://poweronlytransit.com');
+  define('WP_SITEURL','http://poweronlytransit.com');
